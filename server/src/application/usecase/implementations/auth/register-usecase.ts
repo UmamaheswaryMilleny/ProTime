aa361132513736110b4a87e5ee0c4775cf85a1d2
Umamaheswary Milleny <umamaheswarymillini@gmail.com>
2026-01-19 19:44:18 +0530

    added regsiter usecase

---
+++ server/src/application/usecase/implementations/auth/register-usecase.ts
@@ -0,0 +1,35 @@
+import { inject, injectable } from "tsyringe";
+import type { IUserEntity } from "../../../../domain/entities/user.js";
+import type { IUserRepository } from "../../../../domain/repositoryInterface/user/user-repository-interface.js";
+import { hashPassword } from "../../../../shared/utils/bcryptHelper.js";
+import type { IRegisterUsecase } from "../../interfaces/auth/register-usecase-interface.js";
+
+@injectable()
+export class RegisterUsecase implements IRegisterUsecase {
+  constructor(
+    @inject("IUserRepository")
+    private _userRepository: IUserRepository
+  ) {}
+  async execute(data: Partial<IUserEntity>): Promise<void> {
+    if (!data.email ) {
+      throw new Error("email or phone required");
+    }
+
+    const isEmailExists = await this._userRepository.findByEmail(data.email);
+
+    if (isEmailExists) {
+      throw new Error("email already exists");
+    }
+
+ 
+
+    const hashedPassword = await hashPassword(data.password!);
+
+    const dataToSave: Partial<IUserEntity> = {
+      ...data,
+      password: hashedPassword,
+    };
+
+    await this._userRepository.save(dataToSave);
+  }
+}
