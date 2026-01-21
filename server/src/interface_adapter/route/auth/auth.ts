06328e11eda22804d720529911cd16db2ee9c74c
Umamaheswary Milleny <umamaheswarymillini@gmail.com>
2026-01-21 18:27:09 +0530

    added routes

--- server/src/interface_adapter/route/auth/auth.ts
+++ server/src/interface_adapter/route/auth/auth.ts
@@ -3,13 +3,19 @@ import { asyncHandler } from "../../../shared/async-handler.js";
 import { BaseRoute } from "../base-route.js";
 import {
   authController,
-
+  blockedUserMiddleware,
 } from "../../../infrastructure/dependencyinjection/resolve.js";
 import { validationMiddleware } from "../../middlewares/validation-middleware.js";
+import { LoginRequestDTO } from "../../../application/dto/request/login-request-dto.js";
 import { RegisterRequestDTO } from "../../../application/dto/request/register-request-dto.js";
-
+import { AdminLoginRequestDTO } from "../../../application/dto/request/adminlogin-request-dto.js";
+import { ResendOtpUsecase } from "../../../application/usecase/implementations/auth/resendOtp-usecase.js";
 import { SendOtpRequestDTO } from "../../../application/dto/request/sentOtpRequest-dto.js";
 import { VerifyOtpRequestDTO } from "../../../application/dto/request/verifyotp-request-dto.js";
+import { ForgotPasswordRequestDTO } from "../../../application/dto/request/forgot-password-request-dto.js";
+import { ResetPasswordRequestDTO } from "../../../application/dto/request/reset-password-request-dto.js";
+import { VerifyResetTokenRequestDTO } from "../../../application/dto/request/verify-reset-token-request-dto.js";
+import { GoogleAuthRequestDTO } from "../../../application/dto/request/google-auth-request-dto.js";
 
 @injectable()
 export class AuthRoutes extends BaseRoute {
@@ -23,10 +29,20 @@ export class AuthRoutes extends BaseRoute {
       validationMiddleware(RegisterRequestDTO),
       asyncHandler(authController.register.bind(authController))
     );
+    this.router.post(
+      "/login",
+      validationMiddleware(LoginRequestDTO),
+      asyncHandler(authController.login.bind(authController))
+    );
   
 
 
 
+    this.router.post(
+      "/admin/login",
+      validationMiddleware(AdminLoginRequestDTO),
+      asyncHandler(authController.AdminLogin.bind(authController))
+    );
     this.router.post(
       "/send-otp",
       validationMiddleware(SendOtpRequestDTO),
@@ -51,9 +67,35 @@ export class AuthRoutes extends BaseRoute {
     );
   
 
+    this.router.post(
+      "/logout",
+      asyncHandler(authController.logout.bind(authController))
+    );
+
+    this.router.post(
+      "/refresh-token",
+      asyncHandler(authController.refreshToken.bind(authController))
+    );
+
+ 
 
 
 
+    this.router.get(
+      "/verify-reset-token",
+      validationMiddleware(VerifyResetTokenRequestDTO),
+      asyncHandler(authController.verifyResetToken.bind(authController))
+    );
 
+    this.router.post(
+      "/google",
+      // validationMiddleware(GoogleAuthRequestDTO),
+      asyncHandler(authController.googleAuth.bind(authController))
+    );
+
+    // this.router.get(
+    //   "/profile",
+    //   asyncHandler(userController.getProfile.bind(userController))
+    // );
   }
 }
